015d540b8eb7e1429d7731ee8b5ed8cfc01baccb
sneha <nikamsnehal0903@gmail.com>
2025-08-25 13:59:09 +0530

    commit

--- event-platform-frontend/src/app/app.routes.ts
+++ event-platform-frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { EventsListComponent } from './events/events-list';
 import { AuthGuard } from './guards/auth.guard';
 import { SeatBookingComponent } from './events/seat-booking/seat-booking';
 import { ThankYouComponent } from './events/Thank-You/thank-you';
+import { NotFound } from './not-found/not-found';
 
 export const routes: Routes = [
   { path: '', component: EventsListComponent },
@@ -17,5 +18,5 @@ export const routes: Routes = [
     component: SeatBookingComponent,
   },
   { path: 'thank-you', component: ThankYouComponent },
-  { path: '**', redirectTo: '' },
+  { path: '**', component: NotFound },
 ];
